4fc8d009c942ef922e3ac4814176b64edb8453c3
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-04-22 14:31:29 -0400

    app initialization

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <p className="text-6xl text-[#4169e1]">Hello FlashMash</p>
+    // <Button variant="destructive" size="lg">Click Me</Button>
+  );
+}

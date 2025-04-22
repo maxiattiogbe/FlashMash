287196be8ecd1c20e531c23a0539cab79243a94a
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-04-22 16:44:40 -0400

    authentication

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const LandingPage = () => {
   return (
-    <p className="text-white">Landing Page (Unprotected)</p>
+    <div>
+      <p className="text-white">Landing Page (Unprotected)</p>
+      <div>
+        <Link href="/sign-in">
+          <Button>
+            Login
+          </Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 }
 

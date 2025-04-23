05d6f9e03318491ed91b9586d4a445a18a142ff1
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-04-23 14:20:28 -0400

    updated dashboard text

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -34,10 +34,10 @@ const DashboardPage = () => {
         <div>
             <div className="mb-8 space-y-4">
                 <h2 className="text-2xl md:text-4xl font-bold text-center">
-                    Explore the power of AI
+                    Smarter Flashcards, Powered by AI
                 </h2>
                 <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-                    Chat with the smartest AI - Experience the power of AI
+                    Learn smarter. Practice faster. Let FlashMash personalize your flashcard journey.
                 </p>
             </div>
             <div className="px-4 md:px-20 lg:px-32 space-y-4">

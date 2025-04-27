37338dc84df41b7a0fb3571e52c310aa591b3f70
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-04-27 15:24:16 -0400

    made flashcard text smaller on mobile

--- components/fixed-card.tsx
+++ components/fixed-card.tsx
@@ -6,7 +6,7 @@ interface FixedCardProps {
 
 const FixedCard = ({ word }: FixedCardProps) => {
   return (
-    <div className="text-4xl font-semibold w-60 h-40 sm:w-70 sm:h-50 md:w-80 md:h-60 lg:w-120 lg:h-80 border rounded-lg shadow-md bg-white text-[#4169e1] flex items-center justify-center">
+    <div className="text-2xl md:text-4xl font-semibold w-60 h-40 sm:w-70 sm:h-50 md:w-80 md:h-60 lg:w-120 lg:h-80 border rounded-lg shadow-md bg-white text-[#4169e1] flex items-center justify-center">
       {word}
     </div>
   );

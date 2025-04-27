8cbe958b1cb575da270f050f647d99114d1c46e8
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-04-27 15:45:59 -0400

    trial 3 to make stop and next buttons smaller on mobile

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-16 w-40 rounded-lg px-10 has-[>svg]:px-8 sm:h-8 sm:w-20 sm:px-8 sm:has-[>svg]:px-6",
+        lg: "h-8 w-20 rounded-lg px-10 has-[>svg]:px-8 sm:h-16 sm:w-40 sm:px-8 sm:has-[>svg]:px-6",
         icon: "size-9",
       },
     },
